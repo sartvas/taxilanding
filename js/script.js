84c24128cebea85d2487ec77055168ab2d21e84c
Sartvas <sartvas@yandex.ru>
2023-05-25 16:38:22 +0300

    Taxi: адаптивная вёрстка проекта (1200, 991, 747, 524

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-"use strict";
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+    menuItem = document.querySelectorAll('.menu_item'),
+    hamburger = document.querySelector('.hamburger');
 
-const myBtn = document.querySelectorAll('button');
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
 
-const myDiv = document.createElement('div');
-
-myDiv.classList.add('black-div');
-
-
-console.log(myBtn.forEach(item =>{
-    item.onclick = function(){
-        document.querySelector('.reasons_advant').prepend(myDiv);
-        console.log('click')
-    }
-}));
-
-myDiv.innerHTML = '<h1>Hello</h1>'
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        })
+    })
+})
