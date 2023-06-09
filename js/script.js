05db2de92b43699a524e0a465a8340d58f9915bd
Sartvas <sartvas@yandex.ru>
2023-06-09 14:42:16 +0300

    Taxi: добавлены модальные окна

--- js/script.js
+++ js/script.js
@@ -14,4 +14,31 @@ window.addEventListener('DOMContentLoaded', () => {
             menu.classList.toggle('menu_active');
         })
     })
+
+    const dataModal = document.querySelectorAll('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          modalClose = document.querySelector('[data-close]');
+
+    dataModal.forEach(item => {
+        item.addEventListener('click', () =>{
+            modal.style.display = 'block';
+            document.body.style.overflow = 'hidden';
+        })
+    })
+
+    modalClose.addEventListener('click', () => {
+        modal.style.display = 'none';
+        document.body.style.overflow = '';
+    })
+
+    modal.addEventListener('click', (e) => {
+        if(e.target === modal){
+            modal.style.display = 'none';
+            document.body.style.overflow = '';
+        }
+    })
+
+
+
+
 })
